5ced3990f7a64279313200762ecee7be214e210a
dante <dmitry.k@itmedia.su>
2021-02-27 16:35:43 +0300

    init

---
+++ migrations/m210227_113354_create_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m210227_113354_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'email' => $this->string(100),
+            'password' => $this->string(255)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
